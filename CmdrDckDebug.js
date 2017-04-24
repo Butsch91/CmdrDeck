32210e51369a83ce69a3792c9a11e7bd01966ebe
Eric Bouchard <Butsch91@users.noreply.github.com>
2017-04-24 15:29:04 -0400

    Delete CmdrDckDebug.js

--- CmdrDckDebug.js
+++
@@ -1,9 +0,0 @@
-// CmdrDckDebug
-
-function myFunction() {
-  
-  //var TestSht = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Test");
-  //TestSht.getRange(1,2).setValue(actShtName);
-  //TestSht.getRange(2,2).setValue(CardName);
-
-}
